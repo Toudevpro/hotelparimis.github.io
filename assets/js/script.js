e41e8c4511c42465d4aacfc0a7bcc9cd51efaae9
Toudevpro <tou.devpro@gmail.com>
2019-12-07 16:34:12 +0100

    prod test

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// carousel //////////////////////////////////////////
+
+var prev = document.querySelector('div.prev');
+var next = document.querySelector('div.next');
+var index = 0;
+var lis = document.querySelectorAll('ul.slider li');
+var count = lis.length;
+
+prev.addEventListener('click', function() {
+    lis[index].classList.remove('visible');
+    index--;
+    if (index < 0){
+        index = count-1;
+    }
+
+    lis[index].classList.add('visible');
+});
+
+next.addEventListener('click', function() {
+    lis[index].classList.remove('visible');
+    index++;
+    if (index > count-1){
+        index = 0;
+    }
+
+    lis[index].classList.add('visible');
+});
+
+// fin carousel //////////////////////////////////////////
+
+// formulaire reservation //////////////////////////////////////////
+
+(function($){
+    function floatLabel(inputType){
+        $(inputType).each(function(){
+            var $this = $(this);
+            // on focus add cladd active to label
+            $this.focus(function(){
+                $this.next().addClass("active");
+            });
+            //on blur check field and remove class if needed
+            $this.blur(function(){
+                if($this.val() === '' || $this.val() === 'blank'){
+                    $this.next().removeClass();
+                }
+            });
+        });
+    }
+    // just add a class of "floatLabel to the input field!"
+    floatLabel(".floatLabel");
+})(jQuery);
+
+// fin formulaire reservation //////////////////////////////////////////
